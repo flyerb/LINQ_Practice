9c96822ea3ed659992b77653e97be89ce2b0f647
FlyerB <zarandazchi@gmail.com>
2020-05-14 12:19:12 -0500

    2nd problem finsihed

--- LINQ_Practice/Program.cs
+++ LINQ_Practice/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LINQ_Practice
 {
@@ -6,7 +10,51 @@ namespace LINQ_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            IList<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            var result = words.Where(w => w.Contains("bike"));
+
+            foreach( var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+
+            //***************************************************************************
+
+            //returns a copy without a duplicate
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+            var distinct = names.Distinct().ToList();
+            foreach (string name in distinct)
+            {
+                Console.WriteLine(name);
+            }
+
+            //**************************************************************************
+
+
+            IList<string> classGrades = new List<string>(){ "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55"};
+
+
+
+            var lowestDropped = classGrades.Where(c => c.
+
+            
+            Console.WriteLine();
+
+            //lowestDropped.Where(c => c > 69);
+
+            //foreach (var grade in lowestDropped)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //convert string to int-
+            //drop last score ( could use substring)
+
         }
     }
 }
---
+++ LINQ_Practice/Substring.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Practice
+{
+    class Substring
+    {
+        //function that returns all words containing subtring "th"
+
+        List<string> words = new List<string>();
+
+        public Substring()
+        {
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+          
+
+        }
+
+
+       
+
+
+    }
+}
